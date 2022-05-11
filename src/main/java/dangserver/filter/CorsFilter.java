17a3ddbe7b4f5b7b931fa1cc5da88f017fd41cbf
Ethereal-bang <939847757@qq.com>
2022-05-12 00:45:47 +0800

    fix: 跨域

---
+++ src/main/java/dangserver/filter/CorsFilter.java
@@ -0,0 +1,19 @@
+package dangserver.filter;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "CorsFilter")
+@Configuration
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(req, res);
+    }
+}
